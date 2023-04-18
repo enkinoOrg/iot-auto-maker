fc93888e3e1fd249b198d3d15f0bae106dada1b3
SonYoungGyun <dksnqltm117@gmail.com>
2023-04-18 10:34:35 +0900

    json post

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,10 @@
 # define models here
+# import basemodel
+from pydantic import BaseModel
+
+class ProjectModel(BaseModel):
+    projectName: str
+    dbType: str
+    version: float
+    explain: str
+    tableName: list
