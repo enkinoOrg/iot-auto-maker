ed207db04ec2923b719cf563994f93040bbd3394
SonYoungGyun <dksnqltm117@gmail.com>
2023-05-11 10:53:16 +0900

    main.py생성

--- backend/main.py
+++ backend/main.py
@@ -133,11 +133,13 @@
 from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
 from app.models import ProjectModel
+from makeFileFunc.funciton import replace_word_in_file
 from makeFileFunc.funciton import copy_file_content
 from makeFileFunc.funciton import append_text_to_file
 
 ## test용
 import re
+import os
 from pydantic import BaseModel
 
 import shutil
@@ -200,29 +202,26 @@ def make_model(table_list):
 async def copy_postgresql_folder(projectModel: ProjectModel):
     print(projectModel)
     table_list = projectModel.tableName
+    
+    #TODO 기존에 폴더 존재할때 예외처리
     copy_folder('postgresql', 'api_result/postgresql')
+    copy_main_file(projectModel.projectName)
 
 # 1. 기본 폴더 설정 structure
-
-
-
-
-
-
-
-
-
-
-
-
-
 # 폴더를 복사하는 함수
+# 기존에 폴더가 존재하면 예외처리
 def copy_folder(src_folder, dst_folder):
+    if (os.path.isdir(dst_folder)):
+        print("exist")
+        shutil.rmtree(dst_folder)
     shutil.copytree(src_folder, dst_folder)
 
 # 2. main.py를 복사
-def copy_main_file(table_list):
-    copy_file_content('main.py', 'api_result/postgresql/src/main.py')
+def copy_main_file(router_name):
+    copy_file_content('postgresql/txt/main.txt', 'api_result/postgresql/src/app/main.py')
+    replace_word_in_file('api_result/postgresql/src/app/main.py', '{router_name}', router_name)
+
+# --------------------- main.py까지 완료 ---------------------
 
 # 3. db.py를 복사
 def copy_db_file(table_list):
---
+++ backend/postgresql/txt/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api import notes, ping
+from app.db import engine, metadata, database
+
+metadata.create_all(engine)
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["DELETE", "GET", "POST", "PUT"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+# router_name
+app.include_router({router_name}.router, prefix="/{router_name}", tags=["{router_name}"])
