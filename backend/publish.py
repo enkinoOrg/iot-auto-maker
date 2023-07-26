49b3e85161499a8cf54d5719b18021c62650e273
SonYoungGyun <dksnqltm117@gmail.com>
2023-07-27 08:17:57 +0900

    publish.py 추가

---
+++ backend/publish.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import json
+
+json_data = {
+    "RTU_ID": "AF_0001",
+    0: 200,
+    1: 300,
+}
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to broker")
+    else:
+        print("Bad connection Returned code=", rc)
+
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print(str(rc))
+
+
+def on_publish(client, userdata, mid):
+    print("In on_pub callback mid= ", mid)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_publish = on_publish
+
+client.connect("localhost", 1883)
+client.loop_start()
+
+client.publish("device_id/telementry",
+               json.dumps(json_data), 1)
+client.loop_stop()
+client.disconnect()
