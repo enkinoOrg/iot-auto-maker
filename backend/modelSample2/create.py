3e3fd6875fa899d7c87672a362352ec587a1810d
SonYoungGyun <dksnqltm117@gmail.com>
2023-08-07 09:29:42 +0900

    코드 정리

--- backend/modelSample2/create.py
+++
@@ -1,8 +0,0 @@
-async def create(payload: schema_name):
-    query = table_name.insert().values(
-        # table 내용
-        RTU_ID=schema_name.RTU_ID,
- 		RPM=schema_name.RPM,
- 		
-    )
-    return await database.execute(query=query)
