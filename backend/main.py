d73a85ff46ed490983a82c430348b84623fa15be
SonYoungGyun <dksnqltm117@gmail.com>
2023-08-09 14:52:28 +0900

    api result폴더의 이름을 build로 바꿈

--- backend/main.py
+++ backend/main.py
@@ -60,41 +60,41 @@ def copy_folder(src_folder, dst_folder):
 
 # 2. main.py를 복사
 def copy_main_file(router_name):
-    copy_file_content('postgresql/txt/main.txt', 'api_result/postgresql/src/app/main.py')
-    replace_word_in_file('api_result/postgresql/src/app/main.py', '{router_name}', router_name)
+    copy_file_content('postgresql/txt/main.txt', 'build/postgresql/src/app/main.py')
+    replace_word_in_file('build/postgresql/src/app/main.py', '{router_name}', router_name)
     # main.txt.삭제
-    os.remove('api_result/postgresql/src/app/main.txt')
-    os.remove('api_result/postgresql/txt/main.txt')
+    os.remove('build/postgresql/src/app/main.txt')
+    os.remove('build/postgresql/txt/main.txt')
 
 # --------------------- main.py까지 완료 ---------------------
 
 # 3. db.py를 복사
 def copy_db_file(table_list, table_name):
-    copy_file_content('api_result/postgresql/txt/db.txt', 'api_result/postgresql/src/app/db.py')
+    copy_file_content('build/postgresql/txt/db.txt', 'build/postgresql/src/app/db.py')
     content = create_content_db_postgre(table_list)
     print(content)
-    replace_word_in_file('api_result/postgresql/src/app/db.py', 'table_content', content)
-    replace_word_in_file('api_result/postgresql/src/app/db.py', 'schema_name', table_name)
+    replace_word_in_file('build/postgresql/src/app/db.py', 'table_content', content)
+    replace_word_in_file('build/postgresql/src/app/db.py', 'schema_name', table_name)
     # db.txt 삭제
-    os.remove('api_result/postgresql/src/app/db.txt')
-    os.remove('api_result/postgresql/txt/db.txt')
+    os.remove('build/postgresql/src/app/db.txt')
+    os.remove('build/postgresql/txt/db.txt')
 
 # --------------------- db.py까지 완료 ---------------------
 
 # 4. crud.py를 생성
 # 5. crud (create, read, update, delete) 함수를 생성
 def copy_crud_file(table_list, schema_model, schema_db, table_name):
-    copy_file_content('api_result/postgresql/txt/crud.txt', 'api_result/postgresql/src/app/api/crud.py')
-    replace_word_in_file('api_result/postgresql/src/app/api/crud.py', '${schema_model}', schema_model)
-    replace_word_in_file('api_result/postgresql/src/app/api/crud.py', '${schema_name}', schema_db)
-    replace_word_in_file('api_result/postgresql/src/app/api/crud.py', '${table_name}', table_name)
+    copy_file_content('build/postgresql/txt/crud.txt', 'build/postgresql/src/app/api/crud.py')
+    replace_word_in_file('build/postgresql/src/app/api/crud.py', '${schema_model}', schema_model)
+    replace_word_in_file('build/postgresql/src/app/api/crud.py', '${schema_name}', schema_db)
+    replace_word_in_file('build/postgresql/src/app/api/crud.py', '${table_name}', table_name)
 
     # create 함수 생성
-    replace_word_in_file('api_result/postgresql/src/app/api/crud.py', 'create_content', create_crud_content(table_list))
+    replace_word_in_file('build/postgresql/src/app/api/crud.py', 'create_content', create_crud_content(table_list))
 
     # update 함수 생성
-    replace_word_in_file('api_result/postgresql/src/app/api/crud.py', 'update_content', update_crud_content(table_list))
-    os.remove('api_result/postgresql/src/app/api/crud.txt')
+    replace_word_in_file('build/postgresql/src/app/api/crud.py', 'update_content', update_crud_content(table_list))
+    os.remove('build/postgresql/src/app/api/crud.txt')
 
 def create_crud_content(table_list):
     content = ""
@@ -110,19 +110,19 @@ def update_crud_content(table_list):
 
 # model
 def copy_models_file(table_list, schema_model, schema_db):
-    copy_file_content('api_result/postgresql/txt/models.txt', 'api_result/postgresql/src/app/api/models.py')
+    copy_file_content('build/postgresql/txt/models.txt', 'build/postgresql/src/app/api/models.py')
     content = create_content_model_postgre(table_list)
-    replace_word_in_file('api_result/postgresql/src/app/api/models.py', 'model_content', content)
+    replace_word_in_file('build/postgresql/src/app/api/models.py', 'model_content', content)
 
-    replace_word_in_file('api_result/postgresql/src/app/api/models.py', '${schema_model}', schema_model)
-    replace_word_in_file('api_result/postgresql/src/app/api/models.py', '${schema_name}', schema_db)
+    replace_word_in_file('build/postgresql/src/app/api/models.py', '${schema_model}', schema_model)
+    replace_word_in_file('build/postgresql/src/app/api/models.py', '${schema_name}', schema_db)
 
-    os.remove('api_result/postgresql/src/app/api/models.txt')
+    os.remove('build/postgresql/src/app/api/models.txt')
 
 # content 생성
 def copy_routers_file(table_list, schema_model, schema_db):
-    copy_file_content('api_result/postgresql/txt/routers.txt', 'api_result/postgresql/src/app/api/routers.py')
-    os.remove('api_result/postgresql/src/app/api/routers.txt')
+    copy_file_content('build/postgresql/txt/routers.txt', 'build/postgresql/src/app/api/routers.py')
+    os.remove('build/postgresql/src/app/api/routers.txt')
     content = ""
     # models에 있는 내용을 가져와서 content에 추가
 
@@ -130,20 +130,20 @@ def copy_routers_file(table_list, schema_model, schema_db):
         print(f'{table["data_name"]}: {table["data_type"]}')
         content += f'"{table["data_name"]}": payload.{table["data_name"]},\n\t\t'
 
-    replace_word_in_file('api_result/postgresql/src/app/api/routers.py', '${schema_model}', schema_model)
+    replace_word_in_file('build/postgresql/src/app/api/routers.py', '${schema_model}', schema_model)
 
-    replace_word_in_file('api_result/postgresql/src/app/api/routers.py', '${schema_name}', schema_db)
+    replace_word_in_file('build/postgresql/src/app/api/routers.py', '${schema_name}', schema_db)
     
     # TODO : 'object contets 가 맞는지 여부 확인 필요
-    replace_word_in_file('api_result/postgresql/src/app/api/routers.py', 'object contets', content)
+    replace_word_in_file('build/postgresql/src/app/api/routers.py', 'object contets', content)
 
-    replace_word_in_file('api_result/postgresql/src/app/api/routers.py', 'update_content', content)
+    replace_word_in_file('build/postgresql/src/app/api/routers.py', 'update_content', content)
 
 
 def make_mqtt_subscribe_file(sec_key, table_id, table_name):
-    replace_word_to_array('api_result/postgresql/mqtt/src/subscribe.py', '${sec_key}', sec_key)
-    replace_word_in_file('api_result/postgresql/mqtt/src/subscribe.py', '${table_id}', table_id)
-    replace_word_in_file('api_result/postgresql/mqtt/src/subscribe.py', '${table_name}', table_name)
+    replace_word_to_array('build/postgresql/mqtt/src/subscribe.py', '${sec_key}', sec_key)
+    replace_word_in_file('build/postgresql/mqtt/src/subscribe.py', '${table_id}', table_id)
+    replace_word_in_file('build/postgresql/mqtt/src/subscribe.py', '${table_name}', table_name)
 
 # mqtt 서버 생성
 # @app.post("/template") 
@@ -180,7 +180,7 @@ async def make_project_file(getProjectIdModel: GetProjectIdModel):
     print("schema_model : ", schema_model)
     print("schema_db : ", schema_db)
 
-    copy_folder('postgresql', 'api_result/postgresql')
+    copy_folder('postgresql', 'build/postgresql')
     print("=== copy folder end ===")
     copy_main_file(table_name)
     print("=== copy main end ===")
