75b4c8def87ba8acddb6dc1c085e7b80d46749db
plutoin <uleage@gmail.com>
2023-09-08 17:11:15 +0900

    mqtt 서버 테스트

--- dashboard/test-mqtt.js
+++ dashboard/test-mqtt.js
@@ -1,13 +1,5 @@
 const mqtt = require('mqtt')
 
-const options = {
-  host: '192.168.219.103',
-  port: 8080,
-  protocol: 'ws'
-  // username:"",
-  // password:"",
-}
-
 // mqtt 서버로 보내는 코드
 const topic = 'cmd/water1-E8DB84986E61'
 const mqttMessage = '{"uuid":"3a43d665-6ad6-45ee-9e12-077180b045db","relay_name":"water_pump","relay_value":1}'
