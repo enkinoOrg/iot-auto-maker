668f7c6f25ea83e5d3919a6db52a122cd778a5fc
HyeonJeong Moon <cs@enkino.com>
2023-09-08 17:40:12 +0900

    mqtt 에 웹소켓 설정 추가

--- dashboard/test-mqtt.js
+++ dashboard/test-mqtt.js
@@ -1,13 +1,14 @@
-const mqtt = require('mqtt')
+const mqtt = require('mqtt');
 
 // mqtt 서버로 보내는 코드
-const topic = 'cmd/water1-E8DB84986E61'
-const mqttMessage = '{"uuid":"3a43d665-6ad6-45ee-9e12-077180b045db","relay_name":"water_pump","relay_value":1}'
+const topic = 'cmd/water1-E8DB84986E61';
+const mqttMessage =
+  '{"uuid":"3a43d665-6ad6-45ee-9e12-077180b045db","relay_name":"water_pump","relay_value":0}';
 // const client = mqtt.connect(options)
-const client = mqtt.connect('mqtt://192.168.219.103')
+const client = mqtt.connect('mqtt://192.168.219.103');
 
 client.on('connect', () => {
-  console.log('connected' + client.connected)
-  client.publish(topic, mqttMessage)
-  client.end()
-})
+  console.log('connected ' + client.connected);
+  client.publish(topic, mqttMessage);
+  client.end();
+});
