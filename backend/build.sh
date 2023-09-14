5125d5c5474670f69cf617a37c3d78ab49845e43
SonYoungGyun <dksnqltm117@gmail.com>
2023-09-14 16:38:43 +0900

    readme.md에서 window에서 실행시 주의 추가

--- backend/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "json파일의 url을 입력해주세요."
-read JSON_URL
-python3 main.py $JSON_URL
--- backend/postgresql/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "프로젝트 이름을 입력하세요"
-read project_name
-docker compose up -p $project_name -d
