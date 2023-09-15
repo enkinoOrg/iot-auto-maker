917fb0280ebfffa2f2780a6a4588e67b8a10fd8f
plutoin <uleage@gmail.com>
2023-09-15 16:13:31 +0900

    로그 시작, 중지 기능 구현 로그 데이터 csv 파일로 다운로드 기능 구현 angle_x, angle_y 값 상단에서 정보 표시

--- dashboard/pages/r3f-flight/index.jsx
+++ dashboard/pages/r3f-flight/index.jsx
@@ -1,59 +1,169 @@
-import React, { Suspense, useState, useEffect } from 'react';
-import * as mqtt from 'mqtt'; // import everything inside the mqtt module and give it the namespace "mqtt"
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useState, useEffect, useRef } from "react";
+import * as mqtt from "mqtt"; // import everything inside the mqtt module and give it the namespace "mqtt"
+import { saveAs } from "file-saver";
+import { Canvas } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+
+import App from "./App.jsx";
+
 // import './index.css';
 
 function R3FFlight() {
   const [mqttClientReady, setMqttClientReady] = useState(false);
-  const [mqttClient, setMqttClient] = useState(null);
+  // const [mqttClient, setMqttClient] = useState(null);
   const [connectStatus, setConnectStatus] = useState(null);
   const [jsonData, setJsonData] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [logArray, setLogArray] = useState([]);
 
   useEffect(() => {
-    if (mqttClientReady) {
-      mqttClient.on('connect', () => {
-        setConnectStatus('Connected');
-        mqttClient.subscribe('telemetry');
+    let mqttClient;
+
+    const setupMqttClient = () => {
+      mqttClient = mqtt.connect("ws://localhost:8080");
+
+      mqttClient.on("connect", () => {
+        setConnectStatus("Connected");
+        mqttClient.subscribe("telemetry");
       });
-      mqttClient.on('error', (err) => {
-        console.error('Connection error: ', err);
-        setConnectStatus('Connection error: ' + err);
-        mqttClient.unsubscribe('telemetry');
+
+      mqttClient.on("error", (err) => {
+        console.error("Connection error: ", err);
+        setConnectStatus("Connection error: " + err);
+        mqttClient.unsubscribe("telemetry");
         mqttClient.end();
       });
-      mqttClient.on('reconnect', () => {
-        setConnectStatus('Reconnecting');
+
+      mqttClient.on("reconnect", () => {
+        setConnectStatus("Reconnecting");
       });
-      mqttClient.on('message', (topic, message) => {
+
+      mqttClient.on("message", (topic, message) => {
         const jsonData = JSON.parse(message);
-        console.log('on message', jsonData);
-        setJsonData({ angle_x: jsonData['0'], angle_y: jsonData['1'] });
+        setJsonData({ angle_x: jsonData["0"], angle_y: jsonData["1"] });
+        if (isRecording) {
+          setLogArray((prevLogs) => [...prevLogs, jsonData]);
+        }
       });
+    };
+
+    if (mqttClientReady) {
+      setupMqttClient();
     } else {
       setMqttClientReady(true);
-      setMqttClient(mqtt.connect('ws://192.168.219.103:8080'));
     }
 
     return () => {
       if (mqttClient) {
-        mqttClient.unsubscribe('telemetry');
+        mqttClient.unsubscribe("telemetry");
         mqttClient.end();
       }
     };
-  }, [mqttClientReady]);
+  }, [mqttClientReady, isRecording]);
+
+  const calculatePosition = () => {
+    return [10, 10, 0];
+  };
+
+  const startRecording = () => {
+    setIsRecording(true);
+    setLogArray([]);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+  };
+
+  function objectsToCSV(objects) {
+    if (objects.length === 0) {
+      return "";
+    }
+
+    const header = Object.keys(objects[0]);
+    const csvRows = [];
+
+    csvRows.push(header.join(","));
+
+    objects.forEach((object) => {
+      const values = header.map((key) => {
+        const value = object[key];
+        return `"${value}"`;
+      });
+      csvRows.push(values.join(","));
+    });
+
+    const csvString = csvRows.join("\n");
+
+    return csvString;
+  }
+
+  const downloadCSV = () => {
+    if (logArray.length === 0) {
+      alert("다운로드할 데이터가 없습니다.");
+      return;
+    }
+
+    const csvData = objectsToCSV(logArray);
+
+    const blob = new Blob([csvData], { type: "text/csv" });
+    saveAs(blob, "mqtt_data.csv");
+  };
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas shadows>
-        <Suspense fallback={null}>
-          <App
-            angle_x={jsonData?.angle_x || 0}
-            angle_y={jsonData?.angle_y || 0}
-          />
-        </Suspense>
+        <Html
+          prepend
+          calculatePosition={calculatePosition}
+          style={{
+            display: "flex",
+            width: "265px",
+            gap: "10px",
+            flexDirection: "column",
+            padding: "10px",
+            backgroundColor: "white",
+            opacity: "0.8",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              gap: "15px",
+              flexDirection: "row",
+            }}
+          >
+            <span>angle_x: {jsonData?.angle_x}</span>
+            <span>angle_y: {jsonData?.angle_y}</span>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              gap: "10px",
+              flexDirection: "row",
+            }}
+          >
+            <button onClick={startRecording} disabled={isRecording}>
+              로그 시작
+            </button>
+            <button onClick={stopRecording} disabled={!isRecording}>
+              로그 중지
+            </button>
+            <button
+              onClick={downloadCSV}
+              disabled={logArray.length === 0 && !isRecording}
+            >
+              로그 다운로드
+            </button>
+          </div>
+        </Html>
+
+        <App
+          angle_x={jsonData?.angle_x || 0}
+          angle_y={jsonData?.angle_y || 0}
+        />
       </Canvas>
+
       <style jsx global>{`
         html,
         body {
